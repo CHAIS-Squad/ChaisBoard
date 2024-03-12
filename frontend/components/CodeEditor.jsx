621dc7374726d400d5f83bf9356a5ee10cc6c2fd
Caleb Hemphill <calebhemp@gmail.com>
2024-03-12 10:12:42 -0400

    Componentize CodeEditor component

--- frontend/components/CodeEditor.jsx
+++ frontend/components/CodeEditor.jsx
@@ -5,11 +5,28 @@ import ReactCodeMirror from "@uiw/react-codemirror";
 import { langNames, loadLanguage } from "@uiw/codemirror-extensions-langs";
 
 function CodeEditor() {
-  const [code, setCode] = useState("");
   const [language, setLanguage] = useState("python");
   const [indent, setIndent] = useState(2);
   const [autoComplete, setAutoComplete] = useState(true);
 
+  return (
+    <div className="flex flex-col">
+      <EditorHeader
+        setLanguage={setLanguage}
+        setIndent={setIndent}
+        setAutoComplete={setAutoComplete}
+      />
+
+      <EditorDocument
+        language={language}
+        indent={indent}
+        autoComplete={autoComplete}
+      />
+    </div>
+  );
+}
+
+function EditorHeader({ setLanguage, setIndent, setAutoComplete }) {
   function updateLanguage(event) {
     setLanguage(event.target.value);
   }
@@ -23,55 +40,59 @@ function CodeEditor() {
   }
 
   return (
-    <div className="flex flex-col">
-      <div className="flex">
-        <label htmlFor="language">Language</label>
-        <select
-          name="language"
-          id="language"
-          defaultValue={"python"}
-          onChange={updateLanguage}
-        >
-          {langNames.map((lang) => {
-            return (
-              <option key={lang} value={lang}>
-                {lang}
-              </option>
-            );
-          })}
-        </select>
+    <div className="flex">
+      <label htmlFor="language">Language</label>
+      <select
+        name="language"
+        id="language"
+        defaultValue={"python"}
+        onChange={updateLanguage}
+      >
+        {langNames.map((lang) => {
+          return (
+            <option key={lang} value={lang}>
+              {lang}
+            </option>
+          );
+        })}
+      </select>
 
-        <label htmlFor="tabSize">Indent</label>
-        <select
-          name="tabSize"
-          id="tabSize"
-          defaultValue={"2"}
-          onChange={updateIndent}
-        >
-          <option value="2">2 Spaces</option>
-          <option value="4">4 Spaces</option>
-        </select>
+      <label htmlFor="tabSize">Indent</label>
+      <select
+        name="tabSize"
+        id="tabSize"
+        defaultValue={"2"}
+        onChange={updateIndent}
+      >
+        <option value="2">2 Spaces</option>
+        <option value="4">4 Spaces</option>
+      </select>
 
-        <label htmlFor="autoComplete">Completion</label>
-        <input
-          type="checkbox"
-          name="autoComplete"
-          id="autoComplete"
-          defaultChecked
-          onChange={toggleAutoComplete}
-        />
-      </div>
+      <label htmlFor="autoComplete">Completion</label>
+      <input
+        type="checkbox"
+        name="autoComplete"
+        id="autoComplete"
+        defaultChecked
+        onChange={toggleAutoComplete}
+      />
+    </div>
+  );
+}
 
-      <div className="">
-        <ReactCodeMirror
-          value={code}
-          height="300px"
-          theme={"dark"}
-          extensions={[loadLanguage(language)]}
-          basicSetup={{ tabSize: indent, autocompletion: autoComplete }}
-          onChange={setCode}
-        />
-      </div>
+function EditorDocument({ language, indent, autoComplete }) {
+  const [code, setCode] = useState("");
+
+  return (
+    <div className="">
+      <ReactCodeMirror
+        value={code}
+        height="300px"
+        theme={"dark"}
+        extensions={[loadLanguage(language)]}
+        basicSetup={{ tabSize: indent, autocompletion: autoComplete }}
+        onChange={setCode}
+      />
     </div>
   );
 }
