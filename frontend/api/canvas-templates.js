574976cd1775366514bcd285157690bfbc2e9a66
Caleb Hemphill <calebhemp@gmail.com>
2024-03-12 22:04:15 -0400

    Remove commented out code

--- frontend/api/canvas-templates.js
+++ frontend/api/canvas-templates.js
@@ -26,31 +26,6 @@ function useCanvasTemplates() {
     }
   }
 
-  // function getCanvasTemplate(templateName) {
-  //   const templateObjects = [
-      // {
-      //   "id": "linked_list",
-      //   "shapeType": "Circle",
-      //   "x": 220,
-      //   "y": 225.49816409868518,
-      //   "rotation": 117.50656501577862,
-      //   "isDragging": false,
-      // },
-      // {
-      //   "id": "linked_list_1",
-      //   "shapeType": "Circle",
-      //   "x": 811.4166231727013,
-      //   "y": 608.3049074648346,
-      //   "rotation": 133.62903742079496,
-      //   "isDragging": false,
-      // },
-  //   ];
-
-  //   if (templateName === "linked_list") {
-  //     return templateObjects;
-  //   }
-  // }
-
   return { getCanvasTemplate, getCanvasTemplatesList };
 }
 
