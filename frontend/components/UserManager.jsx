fe1ef120e69b8f9d53babe68933f1986f460522d
Caleb Hemphill <calebhemp@gmail.com>
2024-03-15 12:17:29 -0400

    Add inital light/dark themes

--- frontend/components/UserManager.jsx
+++ frontend/components/UserManager.jsx
@@ -1,23 +1,27 @@
 // react
-import { useState } from "react";
-// authentication
-import { useAuth } from "@/contexts/auth";
+import { useState } from 'react';
+// contexts
+import { useAuth } from '@/contexts/auth';
+import { useTheme } from '@/contexts/theme';
 // bootstrap
-import Button from "react-bootstrap/Button";
-import Modal from "react-bootstrap/Modal";
-import Dropdown from "react-bootstrap/Dropdown";
-import SplitButton from "react-bootstrap/SplitButton";
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Dropdown from 'react-bootstrap/Dropdown';
+import SplitButton from 'react-bootstrap/SplitButton';
 
 function UserManager() {
   const [showLoginModal, setShowLoginModal] = useState(false);
   const [showSignUpModal, setShowSignUpModal] = useState(false);
 
   return (
-    <div className="fixed bottom-0">
-      <UserButton
-        setShowLoginModal={setShowLoginModal}
-        setShowSignUpModal={setShowSignUpModal}
-      />
+    <div className='fixed bottom-0'>
+      <div className='flex gap-2'>
+        <UserButton
+          setShowLoginModal={setShowLoginModal}
+          setShowSignUpModal={setShowSignUpModal}
+        />
+        <ThemeButton />
+      </div>
 
       <LoginModal
         show={showLoginModal}
@@ -40,28 +44,26 @@ function UserButton({ setShowLoginModal, setShowSignUpModal }) {
       {user ? (
         <SplitButton
           title={'Logout'}
-          variant="outline-secondary"
-          size="sm"
-          drop="up"
+          variant='outline-secondary'
+          size='sm'
+          drop='up'
           align={{ offset: [0, 0] }}
           onClick={logout}
-          className="mb-2"
+          className='mb-2'
         >
-          <Dropdown.Item eventKey="1">
-            Account
-          </Dropdown.Item>
+          <Dropdown.Item eventKey='1'>Account</Dropdown.Item>
         </SplitButton>
       ) : (
         <SplitButton
-          title="Login"
+          title='Login'
           onClick={() => setShowLoginModal(true)}
-          variant="outline-secondary"
-          size="sm"
-          drop="up"
+          variant='outline-secondary'
+          size='sm'
+          drop='up'
           align={{ offset: [0, 0] }}
-          className="mb-2"
+          className='mb-2'
         >
-          <Dropdown.Item eventKey="1" onClick={() => setShowSignUpModal(true)}>
+          <Dropdown.Item eventKey='1' onClick={() => setShowSignUpModal(true)}>
             Sign Up
           </Dropdown.Item>
         </SplitButton>
@@ -88,29 +90,29 @@ function LoginModal({ show, handleClose }) {
             handleClose();
           }}
         >
-          <div className="mb-3">
-            <label htmlFor="username" className="form-label">
+          <div className='mb-3'>
+            <label htmlFor='username' className='form-label'>
               Username
             </label>
             <input
-              type="text"
-              className="form-control"
-              id="username"
-              name="username"
+              type='text'
+              className='form-control'
+              id='username'
+              name='username'
             />
           </div>
-          <div className="mb-3">
-            <label htmlFor="password" className="form-label">
+          <div className='mb-3'>
+            <label htmlFor='password' className='form-label'>
               Password
             </label>
             <input
-              type="password"
-              className="form-control"
-              id="password"
-              name="password"
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
             />
           </div>
-          <Button type="submit" variant="primary">
+          <Button type='submit' variant='primary'>
             Login
           </Button>
         </form>
@@ -129,6 +131,51 @@ function SignUpModal({ show, handleClose }) {
         <p>Sign Up Coming Soon</p>
       </Modal.Body>
     </Modal>
+  );
+}
+
+function ThemeButton() {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Button
+      variant='outline-secondary'
+      size='sm'
+      className='mb-2'
+      onClick={toggleTheme}
+    >
+      {theme === 'light' ? (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z'
+          />
+        </svg>
+      ) : (
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z'
+          />
+        </svg>
+      )}
+    </Button>
   );
 }
 
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,11 +1,14 @@
 import { AuthProvider } from "@/contexts/auth";
+import { ThemeProvider } from "@/contexts/theme";
 import "@/styles/globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 
 export default function App({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </AuthProvider>
   );
 }
